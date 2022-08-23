e96bc86e73d3077b1f1c9bb671eed372f8955d15
Son La Pham <hello@son-la.co>
2022-08-23 17:02:44 +0200

    logo

---
+++ src/Logo.svelte
@@ -0,0 +1,20 @@
+<div class="logo">
+  <img src="/gc-logo.png" alt="Logo for Gamechangers"/>
+</div>
+
+<style lang="scss">
+  @import "./variables.scss";
+.logo{
+
+  position: fixed;
+  width: auto;
+  height: 70px;
+  text-align: center;
+  top: $SPACE_S;
+  left: $SPACE_S;
+  padding: 0;
+  display: flex;
+
+  user-select: none;
+}
+</style>
